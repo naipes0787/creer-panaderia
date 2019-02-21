c48204eda077c0a992e5f01dbf4c420f1999b3b2
naipes0787 <leandrodavidcosta@gmail.com>
2019-02-21 12:35:28 -0300

    Se agregan productos en el modal de la orden con botones que los agrupan.

--- creer-panaderia/js/main.js
+++ creer-panaderia/js/main.js
@@ -7,6 +7,47 @@ $(function(){
     addClass('active');
   $('body.gallery .navbar-principal ul li#gallery-menu-item').
     addClass('active');
+
+  // Botones productos
+  var btnPanaderia = document.getElementById('btn-panaderia');
+  var btnPasteleria = document.getElementById('btn-pasteleria');
+  var btnOtros = document.getElementById('btn-otros');
+  btnPanaderia.addEventListener('click', showPanaderiaProducts);
+  btnPasteleria.addEventListener('click', showPasteleriaProducts);
+  btnOtros.addEventListener('click', showOtrosProducts);
+
+  function showPanaderiaProducts(){
+    var element = document.getElementById('panaderia');
+    if(element.style.display == "block"){
+      element.style.display = "none";
+    } else {
+      element.style.display = "block";
+      document.getElementById('pasteleria').style.display = "none";
+      document.getElementById('otros').style.display = "none";
+    }
+  }
+
+  function showPasteleriaProducts(){
+    var element = document.getElementById('pasteleria');
+    if(element.style.display == "block"){
+      element.style.display = "none";
+    } else {
+      element.style.display = "block";
+      document.getElementById('panaderia').style.display = "none";
+      document.getElementById('otros').style.display = "none";
+    }
+  }
+
+  function showOtrosProducts(){
+    var element = document.getElementById('otros');
+    if(element.style.display == "block"){
+      element.style.display = "none";
+    } else {
+      element.style.display = "block";
+      document.getElementById('panaderia').style.display = "none";
+      document.getElementById('pasteleria').style.display = "none";
+    }
+  }
 });
 
 $(document).ready(function(){
@@ -18,6 +59,7 @@ $(document).ready(function(){
     $("#mycarousel").carousel('cycle');
   });
 });
+
 function openReservation(){
   $("#reserveModal").modal('show');
 }
--- creer-panaderia/order.php
+++ creer-panaderia/order.php
@@ -42,12 +42,222 @@
               <i class="far fa-clock"></i>
             </div>
           </div>
+
           <div class="form-group has-feedback">
-            <label for="nombre" class="col-sm-2 control-label">
-              Pedido
-            </label>
-            <div class="col-sm-4">
-              <textarea class="form-control" style="width:auto!important!" id="text_pedido" name="text_pedido" rows=7 cols=8 placeholder="Aquí podés detallar tu pedido..." required></textarea>
+            <div class="divTable col-sm-8">
+              <div class="divTableBody">
+                <div class="products-category">
+  		  					<a type="button" id="btn-panaderia" class="btn btn-block btn-products">
+  		            		Panadería
+  		          	</a>
+  		          </div>
+                <div id="panaderia" class="contenedor-productos">
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;PRODUCTO</div>
+                    <div class="divTableCell">&nbsp;PRECIO</div>
+                    <div class="divTableCell">&nbsp;CANTIDAD</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Figaza de manteca</div>
+                    <div class="divTableCell">&nbsp;$30 por kilo</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="figaza" name="nombre" size="4" required> kilos</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan alemán</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="panAleman" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan árabe</div>
+                    <div class="divTableCell">&nbsp;$100 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="panArabe" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan de campo</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan de maíz</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan integral</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan lactal</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan multicereal</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan saborizado</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                </div>
+                <div class="products-category">
+  		  					<a type="button" id="btn-pasteleria" class="btn btn-block btn-products">
+  		            		Pastelería
+  		          	</a>
+  		          </div>
+                <div id="pasteleria" class="contenedor-productos">
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;PRODUCTO</div>
+                    <div class="divTableCell">&nbsp;PRECIO</div>
+                    <div class="divTableCell">&nbsp;CANTIDAD</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Alfajor de maicena</div>
+                    <div class="divTableCell">&nbsp;$30 por kilo</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> kilos</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Budín</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Bocado de coco y ddl</div>
+                    <div class="divTableCell">&nbsp;$100 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Dona</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Galletita de avena</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Galletita de manteca</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Medialuna de grasa</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Medialuna de manteca</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan de leche</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pastafrola</div>
+                    <div class="divTableCell">&nbsp;$30 por kilo</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> kilos</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Scon</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Torta de manzana</div>
+                    <div class="divTableCell">&nbsp;$100 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Torta de ricota</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                </div>
+                <div class="products-category">
+  		  					<a type="button" id="btn-otros" class="btn btn-block btn-products">
+  		            		Otros
+  		          	</a>
+  		          </div>
+                <div id="otros" class="contenedor-productos">
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;PRODUCTO</div>
+                    <div class="divTableCell">&nbsp;PRECIO</div>
+                    <div class="divTableCell">&nbsp;CANTIDAD</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Bizcochos de grasa</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Chipá</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Chips</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Cremona</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Cuernitos</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Focacha</div>
+                    <div class="divTableCell">&nbsp;$30 por kilo</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> kilos</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Galletitas de cereal</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Galletitas de jengibre</div>
+                    <div class="divTableCell">&nbsp;$100 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pan dulce</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Panettone</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Pizzetas</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Prepizza</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                  <div class="divTableRow">
+                    <div class="divTableCell">&nbsp;Stollen</div>
+                    <div class="divTableCell">&nbsp;$10 por unidad</div>
+                    <div class="divTableCell">&nbsp;<input type="input" class="products-input" id="nombre" name="nombre" size="4" required> unidades</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div class="form-group">
